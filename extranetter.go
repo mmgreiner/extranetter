e6f8e754574766ed3102f18aeaf2bafe06c7c36e
mmgreiner <mmgreiner@bluewin.ch>
2024-03-19 23:09:07 +0100

    updated dependencies

--- extranetter.go
+++ extranetter.go
@@ -13,62 +13,167 @@ import (
 	"strings"
 
 	"github.com/gocolly/colly"
+	"github.com/subosito/gotenv"
 )
 
 // http://go-colly.org/docs/introduction/start/
 
-func main() {
-	downloadDir := flag.String("download", "./downloads/", "folder where to download all the files to")
-	var user, pwd, top, folder string
-	flag.StringVar(&user, "user", "", "user name for extranet, example markus.greiner@pfarrei-aarau.ch")
-	flag.StringVar(&pwd, "password", "", "password of extranet")
-	flag.StringVar(&top, "top", "home",
-		"top folder where to start scraping, like 'home', 'personal', 'sitzungen', 'archiv_sitzungen', 'budget', ...")
-	flag.StringVar(&folder, "folder", "", "folder inside top-folder which to download")
-	//tree := flag.Bool("index", false, "show the tree of the folders")
-	silent := flag.Bool("silent", false, "print all files, but does not download them")
-	debugging := flag.Bool("debug", false, "print debugging messages")
+const (
+	pastoralraum = "https://extranet.pastoralraum-aarau.ch"
+	media        = pastoralraum + "/media/web/extranet.pastoralraum-aarau.ch/media/"
+	itds         = "https://secure.itds.ch/itds-auth/login.jsp?redirectTo=https%3A%2F%2Fextranet.pastoralraum-aarau.ch%2F"
+	//pfarraarau = "https://aarau.pastoralraum-aarau.ch/de/home"
+	helpText = `Downloads files from the extranet of Pastoralraum Aarau
+Copyright Pastoralraum Aarau, contact: markus.greiner@pfarrei-aarau.ch
+Two main commands:
+- top: starts downloading at the top level of pastoralraum
+- link: you have to give a direct link to a subfolder (copied from the browser)
+`
+)
 
+type Options struct {
+	// common flags
+	download string
+	user     string
+	password string
+	silent   bool
+	debug    bool
+	isTop    bool
+	// only with isTop
+	top    string
+	folder string
+	// only with link
+	url      string
+	startUrl string
+}
+
+func handleOptions() (Options, error) {
+	linkCmd := flag.NewFlagSet("link", flag.ContinueOnError)
+	topCmd := flag.NewFlagSet("top", flag.ContinueOnError)
+
+	var downloadDir, user, pwd, top, folder, startUrl string
+	var silent, debugging bool
+
+	// options only for link
+	sampleStartUrl := "https://extranet.pastoralraum-aarau.ch/de/sitzungen?waxmlc_dbFolder_2338099=2532705"
+	linkCmd.StringVar(&startUrl, "url", "", "url where to start downloading, copied from browser, ex "+sampleStartUrl)
+
+	// options only for top
+	topCmd.StringVar(&top, "top", "home",
+		"top folder where to start scraping, like 'home', 'personal', 'sitzungen', 'archiv_sitzungen', 'budget', ...")
+	topCmd.StringVar(&folder, "folder", "", "folder inside top-folder which to download")
+
+	// common sub commands
+	for _, cmd := range []*flag.FlagSet{linkCmd, topCmd} {
+		cmd.StringVar(&downloadDir, "download", "./downloads/", "folder where to download all the files to")
+		cmd.StringVar(&user, "user", "", "user name for extranet, example markus.greiner@pfarrei-aarau.ch")
+		cmd.StringVar(&pwd, "password", "", "password of extranet")
+
+		cmd.BoolVar(&silent, "silent", false, "print all files, but does not download them")
+		cmd.BoolVar(&debugging, "debug", false, "print debugging messages")
+	}
 	flag.Usage = func() {
 		w := flag.CommandLine.Output()
-		fmt.Fprintln(w, "Downloads files from the extranet of Pastoralraum Aarau")
-		fmt.Fprintln(w, "Copyright Pastoralraum Aarau, contact: markus.greiner@pfarrei-aarau.ch")
+		fmt.Fprint(w, helpText)
 		fmt.Fprintln(w, "Options:")
+		topCmd.Usage()
+		linkCmd.Usage()
 		flag.PrintDefaults()
 	}
-	flag.Parse()
 
-	if user == "" || pwd == "" {
+	gotenv.Load(".env")
+
+	checkUser := func() error {
+		// allow to set via environment variables
+		if user == "" {
+			user = os.Getenv("user")
+		}
+		if pwd == "" {
+			pwd = os.Getenv("password")
+		}
+
+		if user == "" || pwd == "" {
+			return fmt.Errorf("user and password cannot be empty")
+		} else {
+			return nil
+		}
+	}
+
+	if len(os.Args) < 2 {
+		return Options{}, fmt.Errorf("no subcommands given")
+	}
+	switch os.Args[1] {
+	case "top":
+		topCmd.Parse(os.Args[2:])
+		topUrl, err := url.JoinPath(pastoralraum, "de", top)
+		if err != nil {
+			return Options{}, err
+		}
+		if err := checkUser(); err != nil {
+			return Options{}, err
+		}
+		return Options{
+			download: downloadDir,
+			user:     user,
+			password: pwd,
+			silent:   silent,
+			debug:    debugging,
+			top:      top,
+			folder:   folder,
+			startUrl: topUrl,
+		}, nil
+	case "link":
+		linkCmd.Parse(os.Args[2:])
+		if err := checkUser(); err != nil {
+			return Options{}, err
+		}
+		return Options{
+			download: downloadDir,
+			user:     user,
+			password: pwd,
+			silent:   silent,
+			debug:    debugging,
+			startUrl: startUrl,
+		}, nil
+	case "-h", "--h", "-help", "--help":
 		flag.Usage()
-		println("username and password are required, aborting")
-		os.Exit(1)
+		os.Exit(0)
+		return Options{}, nil
+	default:
+		return Options{}, fmt.Errorf("wrong subcommand %s", os.Args[1])
+	}
+}
+
+// https://aarau.pastoralraum-aarau.ch/de/intern/okp
+
+func main() {
+
+	options, err := handleOptions()
+	if err != nil {
+		panic(err)
 	}
 
 	// handle logging
 	level := slog.LevelInfo
-	if *debugging {
+	if options.debug {
 		level = slog.LevelDebug
 	}
 	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: level}))
 	slog.SetDefault(logger)
 
-	const (
-		pastoralraum = "https://extranet.pastoralraum-aarau.ch"
-		media        = pastoralraum + "/media/web/extranet.pastoralraum-aarau.ch/media/"
-		itds         = "https://secure.itds.ch/itds-auth/login.jsp?redirectTo=https%3A%2F%2Fextranet.pastoralraum-aarau.ch%2F"
-	)
+	// define starting position
 
 	visited := make(map[string]bool)
 
 	c := colly.NewCollector()
 
-	err := c.Post(itds, map[string]string{"username": user, "password": pwd})
+	err = c.Post(itds, map[string]string{"username": options.user, "password": options.password})
 	if err != nil {
 		panic(err)
 	}
 
 	// only parse the menu when the top is home
-	if top == "home" {
+	if options.top == "home" {
 		c.OnHTML("li.jwa_menu_entry a[href]", func(e *colly.HTMLElement) {
 			link := e.Attr("href")
 			//fmt.Println("MENU", e.Text, link) // /de/sitzungen
@@ -111,7 +216,7 @@ func main() {
 			relpath, _ = url.PathUnescape(relpath)
 
 			// move to next node if the relpath does not contain the folder we are looking for
-			if !strings.Contains(relpath, folder) {
+			if !strings.Contains(relpath, options.folder) {
 				return
 			}
 			// if it doesnt have an extension or has a strange extension, add pdf
@@ -121,9 +226,9 @@ func main() {
 			}
 
 			// make the directory if it does not exist
-			fullpath := filepath.Join(*downloadDir, relpath)
+			fullpath := filepath.Join(options.download, relpath)
 			fulldir := filepath.Dir(fullpath)
-			if *silent {
+			if options.silent {
 				fmt.Println("seeing:", relpath)
 			} else {
 				if err := os.MkdirAll(fulldir, 0777); err != nil {
@@ -139,15 +244,9 @@ func main() {
 		}
 	})
 
-	topUrl, err := url.JoinPath(pastoralraum, "de", top)
-	if err != nil {
-		panic(err)
-	}
-	slog.Info("starting", "topUrl", topUrl)
-	c.Visit(topUrl)
+	slog.Info("starting", "starturl", options.startUrl)
+	c.Visit(options.startUrl)
 
 	//c.Visit("https://extranet.pastoralraum-aarau.ch/de/adressen_behoerden?waxmlc_dbFolder_2338113=1636817")
 	slog.Info("Done")
 }
-
-// https://extranet.pastoralraum-aarau.ch/de/archiv_sitzungen?waxmlc_dbFolder_2338127=1807677
